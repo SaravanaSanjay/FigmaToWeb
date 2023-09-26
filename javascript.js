d0c3f91bd03edfe74fcf61f63ecaa2cad8b11d3a
SaravanaSanjay <sachinsanjay09522@gmail.com>
2023-09-26 11:13:47 +0530

    added js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function toggle(){
+  var blur = document.getElementById('blur');
+  blur.classList.toggle('active');
+  var popup = document.getElementById('popup');
+  popup.classList.toggle('active');
+}
+
+const changeText = document.querySelector("#change-text");
+
+  changeText.addEventListener("click", function() {
+    changeText.textContent = "Helloooooooooooooooo Raviii BrooooooooooooOoOOooooOooooOOooOoooOooOooooOOoOoOOoooooooo";
+  });
+  
+    function areyouwilling(){
+      alert("Thanks for Creating Account !");
+    }
+    function choosetheplan() {
+  alert("Here the best plan!");
+    }
